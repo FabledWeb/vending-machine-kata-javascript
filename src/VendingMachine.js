4864dfa04e36697b5d69e3a2549fc636f6fdb287
Eddie Canales <eddiecanales@gmail.com>
2015-06-24 23:58:04 -0400

    decided I won't need any attrs other than value on coins and can refactor a bit

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,9 +1,9 @@
 (function() {
   var VendingMachine = VendingMachine || {};
   var COINS = {
-    nickel:  { value: 0.05 },
-    dime:    { value: 0.10 },
-    quarter: { value: 0.25 }
+    nickel:  0.05,
+    dime:    0.10,
+    quarter: 0.25
   };
 
   VendingMachine.init = function() {
@@ -24,10 +24,11 @@
     this._change = change;
   };
   VendingMachine._isCoinAcceptable = function(coin) {
-    return COINS[coin] ? true : false;
+    //only accept coins that have value to me
+    return this._valueOfCoin(coin) > 0 ? true : false;
   };
   VendingMachine._valueOfCoin = function(coin) {
-    return (COINS[coin] || {value:0}).value;
+    return COINS[coin] || 0;
   };
 
   //export
